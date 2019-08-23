76b03ebc0c3e670cefa9278541003b83671568fa
SuperStulle007 <48865957+SuperStulle007@users.noreply.github.com>
2019-08-23 19:57:14 +0200

    2 Stupid 4 libs

--- src/SuperStulle007/UltraCapes/libs/jojoe77777/FormAPI/Form.php
+++ src/SuperStulle007/UltraCapes/libs/jojoe77777/FormAPI/Form.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace jojoe77777\FormAPI;
+namespace SuperStulle007\UltraCapes\libs\jojoe77777\FormAPI;
 
 use pocketmine\form\Form as IForm;
 use pocketmine\Player;
