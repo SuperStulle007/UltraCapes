0be4d10d6e63cd915a3e3bfcfc0dace7dabd88cb
KMatuszak <polskinosacz1337@gmail.com>
2020-02-24 22:12:35 +0100

    unfuck code style

--- src/SuperStulle007/UltraCapes/libs/jojoe77777/FormAPI/FormAPI.php
+++ src/SuperStulle007/UltraCapes/libs/jojoe77777/FormAPI/FormAPI.php
@@ -1,40 +1,44 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace SuperStulle007\UltraCapes\libs\jojoe77777\FormAPI;
 
 use pocketmine\plugin\PluginBase;
 
-class FormAPI extends PluginBase{
+class FormAPI extends PluginBase
+{
 
     /**
-     * @deprecated
-     *
      * @param callable|null $function
      * @return CustomForm
+     * @deprecated
+     *
      */
-    public function createCustomForm(?callable $function = null) : CustomForm {
+    public function createCustomForm(?callable $function = null): CustomForm
+    {
         return new CustomForm($function);
     }
 
     /**
-     * @deprecated
-     *
      * @param callable|null $function
      * @return SimpleForm
+     * @deprecated
+     *
      */
-    public function createSimpleForm(?callable $function = null) : SimpleForm {
+    public function createSimpleForm(?callable $function = null): SimpleForm
+    {
         return new SimpleForm($function);
     }
 
     /**
-     * @deprecated
-     *
      * @param callable|null $function
      * @return ModalForm
+     * @deprecated
+     *
      */
-    public function createModalForm(?callable $function = null) : ModalForm {
+    public function createModalForm(?callable $function = null): ModalForm
+    {
         return new ModalForm($function);
     }
 }
